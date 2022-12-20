eb638ed87ba624ae746901dc219d045b05c91c24
Joel Hook <70439055+lightchipster@users.noreply.github.com>
2022-12-20 22:05:52 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,27 +1,34 @@
-// Initialize variables for the first two terms of the Fibonacci sequence
-let term1 = 1;
-let term2 = 2;
+// Set the number for which we want to find the largest prime factor
+const number = 600851475143;
 
-// Initialize the sum to the value of the second term (since it's an even number)
-let sum = term2;
+// Initialize the largest prime factor to 2 (since it's the smallest prime number)
+let largestPrimeFactor = 2;
 
-// Set the maximum value for the Fibonacci sequence
-const maxValue = 4000000;
-
-// Calculate the next term in the Fibonacci sequence until we reach the maximum value
-while (term1 + term2 <= maxValue) {
-  // Calculate the next term in the sequence
-  let nextTerm = term1 + term2;
-  
-  // If the next term is even, add it to the sum
-  if (nextTerm % 2 == 0) {
-    sum += nextTerm;
-  }
-  
-  // Set the first term to the second term and the second term to the next term
-  term1 = term2;
-  term2 = nextTerm;
+// Divide the number by 2 until it's no longer divisible
+while (number % 2 == 0) {
+  number /= 2;
 }
 
-// Display the final sum in the HTML element with id "sum"
-document.getElementById("sum").innerHTML = sum;
+// Start checking for prime factors at 3
+let divisor = 3;
+
+// Keep checking for prime factors until the divisor is greater than the number
+while (divisor <= number) {
+  // If the number is divisible by the divisor, divide it by the divisor and update the largest prime factor
+  if (number % divisor == 0) {
+    number /= divisor;
+    largestPrimeFactor = divisor;
+  }
+  // Otherwise, move on to the next possible divisor (skip even numbers)
+  else {
+    divisor += 2;
+  }
+}
+
+// If the number is still greater than 2, it's the largest prime factor
+if (number > 2) {
+  largestPrimeFactor = number;
+}
+
+// Display the largest prime factor in the HTML element with id "largest"
+document.getElementById("largest").innerHTML = largestPrimeFactor;
