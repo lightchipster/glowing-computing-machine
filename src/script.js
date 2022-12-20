d445c7ea934d5e53ae416201ed2f92b3cea9a4cd
Joel Hook <70439055+lightchipster@users.noreply.github.com>
2022-12-20 21:56:01 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,12 +1,26 @@
-// Initialize sum to 0
-let sum = 0;
+// Initialize variables for the first two terms of the Fibonacci sequence
+let term1 = 1;
+let term2 = 2;
 
-// Iterate through all numbers from 1 to 999
-for (let i = 1; i < 1000; i++) {
-  // If the number is a multiple of 3 or 5, add it to the sum
-  if (i % 3 == 0 || i % 5 == 0) {
-    sum += i;
+// Initialize the sum to the value of the second term (since it's an even number)
+let sum = term2;
+
+// Set the maximum value for the Fibonacci sequence
+const maxValue = 4000000;
+
+// Calculate the next term in the Fibonacci sequence until we reach the maximum value
+while (term1 + term2 <= maxValue) {
+  // Calculate the next term in the sequence
+  let nextTerm = term1 + term2;
+  
+  // If the next term is even, add it to the sum
+  if (nextTerm % 2 == 0) {
+    sum += nextTerm;
   }
+  
+  // Set the first term to the second term and the second term to the next term
+  term1 = term2;
+  term2 = nextTerm;
 }
 
 // Display the final sum in the HTML element with id "sum"
